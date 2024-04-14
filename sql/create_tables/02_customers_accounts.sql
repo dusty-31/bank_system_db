6acba9a88c7d78c1acfac733d563b8121ddb76ea
Yuriy Rybak <jura.ryback007@gmail.com>
2024-04-14 16:47:50 +0300

    Updated the `customer_detail`, `account` tables and added insert data for customers and their accounts.

--- sql/create_tables/02_customers_accounts.sql
+++ sql/create_tables/02_customers_accounts.sql
@@ -13,8 +13,8 @@ CREATE TABLE Bank_System.customer_detail
     phone                  VARCHAR(15)                    NOT NULL,
     social_security_number VARCHAR(10)                    NOT NULL,
     occupation             VARCHAR(50)                    NOT NULL,
-    date_of_registration   DATE                           NOT NULL,
-    date_of_last_update    DATE                           NOT NULL,
+    date_of_registration   DATE DEFAULT (CURRENT_DATE),
+    date_of_last_update    DATE DEFAULT NULL,
     email                  VARCHAR(30),
 
     customer_id            INT                            NOT NULL UNIQUE,
@@ -31,22 +31,13 @@ CREATE TABLE Bank_System.account
 (
     account_id             INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     balance                DECIMAL(10, 2)                 NOT NULL,
-    date_of_creation       DATE                           NOT NULL,
-    date_of_last_operation DATE                           NOT NULL,
+    date_of_creation       DATE DEFAULT (CURRENT_DATE)    NOT NULL,
+    date_of_last_operation DATE DEFAULT NULL,
     date_of_closing        DATE DEFAULT NULL,
 
-
     account_type_id        INT                            NOT NULL,
-    FOREIGN KEY (account_type_id) REFERENCES Bank_System.account_type (account_type_id)
-);
+    FOREIGN KEY (account_type_id) REFERENCES Bank_System.account_type (account_type_id),
 
-
-CREATE TABLE Bank_System.customer_accounts
-(
-    customer_id INT,
-    account_id  INT,
-    PRIMARY KEY (customer_id, account_id),
-
-    FOREIGN KEY (customer_id) REFERENCES Bank_System.customer (customer_id),
-    FOREIGN KEY (account_id) REFERENCES Bank_System.account (account_id)
+    customer_id            INT                            NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES Bank_System.customer (customer_id)
 );
---
+++ sql/insert_data/02_customers_accounts.sql
@@ -0,0 +1,24 @@
+INSERT INTO Bank_System.customer(first_name, last_name)
+VALUES ('John', 'Doe'),
+       ('Jade', 'Redfield'),
+       ('Chris', 'Cross');
+
+INSERT INTO Bank_System.customer_detail(date_of_birth, address, phone, social_security_number, occupation, email,
+                                        customer_id)
+VALUES ('1990-01-01', '123 Main St, New York, NY', '123-456-7890', '0000000001', 'Software Engineer',
+        'john_doe@gmail.com', 1),
+       ('1995-02-02', '456 Elm St, New York, NY', '234-567-8901', '0000000002', 'Doctor', 'jade_redfield@gmail.com',
+        2),
+       ('1985-03-03', '789 Oak St, New York, NY', '345-678-9012', '0000000003', 'Lawyer', 'chris_cross@gmail.com', 3);
+
+INSERT INTO Bank_System.account_type(type)
+VALUES ('Checking'),
+       ('Savings'),
+       ('Credit Card'),
+       ('Loan');
+
+INSERT INTO Bank_System.account(balance, account_type_id, customer_id)
+VALUES (1000, 1, 1),
+       (2000, 2, 2),
+       (3000, 3, 2),
+       (4000, 4, 3);
